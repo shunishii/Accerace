32002f52102fa5497d54e5a70d1f4cbc82534181
Shun Ishii <ishiishun10@gmail.com>
2020-03-08 10:44:47 +0900

    Initial commit.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public TextMeshProUGUI gameoverText;
+    public TextMeshProUGUI timeText;
+    public Button startButton;
+    public Button goTitleButton;
+    public bool isGameActive;
+    private float xRange = 2.0f;
+    private float spawnRate = 1.0f;
+    private float countTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.sleepTimeout = SleepTimeout.SystemSetting;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameActive)
+        {
+            countTime += Time.deltaTime;
+            timeText.SetText("Time: " + (Mathf.Round(countTime * 10) / 10).ToString("F1"));
+        }
+    }
+
+    public void GameStart()
+    {
+        isGameActive = true;
+        startButton.gameObject.SetActive(false);
+        countTime = 0.0f;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        StartCoroutine(SpawnBall());
+    }
+
+    IEnumerator SpawnBall()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            if (spawnRate > 0.3f)
+            {
+                spawnRate -= 0.01f;
+            }
+            float xPos = Random.Range(-xRange, xRange);
+            Instantiate(ballPrefab, new Vector3(xPos, -10, 7), transform.rotation);
+        }
+    }
+
+    public void Gameover()
+    {
+        gameoverText.gameObject.SetActive(true);
+        goTitleButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void GoTitle()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 30.0f;
+    private float rangeX = 2.0f;
+    private float rangeZ = 4.3f;
+    public bool isGameover = false;
+    public Button resetButton;
+    public GameManager gameManager;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 val = Input.acceleration;
+        Vector3 dir = Vector3.zero;
+
+        dir.x = val.x;
+        dir.z = val.y;
+
+        if ((transform.position.x > rangeX && dir.x > 0) || (transform.position.x < -rangeX && dir.x < 0))
+        {
+            dir.x = 0;
+        }
+
+        if ((transform.position.z > rangeZ && dir.z > 0) || (transform.position.z < -rangeZ && dir.z < 0))
+        {
+            dir.z = 0;
+        }
+
+        if (gameManager.isGameActive)
+        {
+            dir *= Time.deltaTime;
+            transform.position += (dir * speed);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            isGameover = true;
+            playerRb.AddForce(Vector3.up * 8, ForceMode.Impulse);
+            playerRb.AddTorque(Vector3.right * speed, ForceMode.Impulse);
+            gameManager.Gameover();
+        }
+    }
+}
