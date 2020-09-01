515ace44b34c550747969e7e81b39290921127e1
ShunIshii <ishiishun@gmail.com>
2020-09-01 22:08:27 +0900

    Stage repeat

---
+++ Assets/Scripts/StageController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float speed = 30;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.z / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+        if (transform.position.z < startPos.z - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
