1448aa1a74734964e1751e50a86f3785b9eecb21
ShunIshii <ishiishun@gmail.com>
2020-09-02 22:58:05 +0900

    Key move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,7 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 30.0f;
-    private float rangeX = 2.0f;
-    private float rangeZ = 4.3f;
+    private float rangeX = 1.8f;
     public bool isGameover = false;
     public Button resetButton;
     public GameManager gameManager;
@@ -27,17 +26,24 @@ public class PlayerController : MonoBehaviour
         Vector3 val = Input.acceleration;
         Vector3 dir = Vector3.zero;
 
+#if UNITY_EDITOR
+        Vector3 arrow = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            arrow.x = -0.1f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            arrow.x = 0.1f;
+        }
+        val = arrow;
+#endif
+
         dir.x = val.x;
-        dir.z = val.y;
 
         if ((transform.position.x > rangeX && dir.x > 0) || (transform.position.x < -rangeX && dir.x < 0))
         {
             dir.x = 0;
-        }
-
-        if ((transform.position.z > rangeZ && dir.z > 0) || (transform.position.z < -rangeZ && dir.z < 0))
-        {
-            dir.z = 0;
         }
 
         if (gameManager.isGameActive)
